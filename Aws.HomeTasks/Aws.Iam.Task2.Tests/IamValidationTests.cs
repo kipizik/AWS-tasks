58ff312aed4f8ce0d7de04f04ed20ed8dfb41ad5
Volodymyr Neshta <Volodymyr_Neshta@epam.com>
2023-12-10 00:11:29 +0200

    Add more tests

--- Aws.HomeTasks/Aws.Iam.Task2.Tests/IamValidationTests.cs
+++ Aws.HomeTasks/Aws.Iam.Task2.Tests/IamValidationTests.cs
@@ -1,16 +1,20 @@
 using Amazon.IdentityManagement;
 using Amazon.IdentityManagement.Model;
-using Amazon.Runtime;
 using Aws.Iam.Task2.Tests.Helpers;
 using Aws.Iam.Task2.Tests.Models;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using Newtonsoft.Json;
 using System.Web;
 
 namespace Aws.Iam.Task2.Tests;
 
-public class Tests
+public class IamValidationTests
 {
+    private const string FullAccessPolicyEC2 = "FullAccessPolicyEC2";
+    private const string FullAccessPolicyS3 = "FullAccessPolicyS3";
+    private const string ReadAccessPolicyS3 = "ReadAccessPolicyS3";
+
     private AmazonIdentityManagementServiceClient iamClient;
 
     [SetUp]
@@ -24,7 +28,7 @@ public class Tests
     {
         var expectedPoliciesByName = new Dictionary<string, PolicyModel>
         {
-            ["FullAccessPolicyEC2"] = new PolicyModel
+            [FullAccessPolicyEC2] = new PolicyModel
             {
                 Statement = new[]
                 {
@@ -36,7 +40,7 @@ public class Tests
                     }
                 }
             },
-            ["FullAccessPolicyS3"] = new PolicyModel
+            [FullAccessPolicyS3] = new PolicyModel
             {
                 Statement = new[]
                 {
@@ -48,7 +52,7 @@ public class Tests
                     }
                 }
             },
-            ["ReadAccessPolicyS3"] = new PolicyModel
+            [ReadAccessPolicyS3] = new PolicyModel
             {
                 Statement = new[]
                 {
@@ -85,6 +89,72 @@ public class Tests
         }
 
         returnedPoliciesByArn.Values.Should().BeEquivalentTo(expectedPoliciesByName.Values);
+    }
+
+    [Test]
+    public async Task Validate_Attached_Policy_Roles()
+    {
+        var expectedRolesByPolicyName = new Dictionary<string, string>
+        {
+            [FullAccessPolicyEC2] = "FullAccessRoleEC2",
+            [FullAccessPolicyS3] = "FullAccessRoleS3",
+            [ReadAccessPolicyS3] = "ReadAccessRoleS3"
+        };
+
+        using (new AssertionScope())
+        {
+            foreach (var pair in expectedRolesByPolicyName)
+            {
+                var attachedRolePoliciesResponse = await iamClient.ListAttachedRolePoliciesAsync(new ListAttachedRolePoliciesRequest { RoleName = pair.Value });
+
+                attachedRolePoliciesResponse.AttachedPolicies.Should().HaveCount(1);
+                attachedRolePoliciesResponse.AttachedPolicies.Single().PolicyName.Should().Be(pair.Key);
+            }
+        }
+    }
+
+    [Test]
+    public async Task Validate_Attached_Policy_UserGroups()
+    {
+        var expectedGroupsByPolicyName = new Dictionary<string, string>
+        {
+            [FullAccessPolicyEC2] = "FullAccessGroupEC2",
+            [FullAccessPolicyS3] = "FullAccessGroupS3",
+            [ReadAccessPolicyS3] = "ReadAccessGroupS3"
+        };
+
+        using (new AssertionScope())
+        {
+            foreach (var pair in expectedGroupsByPolicyName)
+            {
+                var attachedRolePoliciesResponse = await iamClient.ListAttachedGroupPoliciesAsync(new ListAttachedGroupPoliciesRequest { GroupName = pair.Value });
+
+                attachedRolePoliciesResponse.AttachedPolicies.Should().HaveCount(1);
+                attachedRolePoliciesResponse.AttachedPolicies.Single().PolicyName.Should().Be(pair.Key);
+            }
+        }
+    }
+
+    [Test]
+    public async Task Validate_Created_Users()
+    {
+        var expectedUsersByGroupName = new Dictionary<string, string>
+        {
+            ["FullAccessUserEC2"] = "FullAccessGroupEC2",
+            ["FullAccessUserS3"] = "FullAccessGroupS3",
+            ["ReadAccessUserS3"] = "ReadAccessGroupS3"
+        };
+
+        using (new AssertionScope())
+        {
+            foreach (var pair in expectedUsersByGroupName)
+            {
+                var getGroupResponse = await iamClient.GetGroupAsync(new GetGroupRequest { GroupName = pair.Value });
+
+                getGroupResponse.Users.Should().HaveCount(1);
+                getGroupResponse.Users.Single().UserName.Should().Be(pair.Key);
+            }
+        }
     }
 
     private static string GetPolicyDocumentAsJson(PolicyVersion policyVersion)
